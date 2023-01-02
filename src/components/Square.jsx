9c9e6809ebfe55abc247bf06d94ba0d2b5098e6b
SahtraRG <sahtra01@gmail.com>
2023-01-02 16:12:35 -0700

    create square component
    Co-authored-by: Logan Ramos <Renzlawr@users.noreply.github.com>

---
+++ src/components/Square.jsx
@@ -0,0 +1,17 @@
+import 'react'
+import '../stylesheets/dot.scss'
+
+const Square = () => {
+  const dotSelect = () => {
+
+  }
+
+  return (
+    <div className='square'>
+      <div className='dot'></div>
+      <div id='vertical-line'></div>
+      <div id='Squareizontal-line'></div>
+    </div>
+  )
+}
+export default Square
